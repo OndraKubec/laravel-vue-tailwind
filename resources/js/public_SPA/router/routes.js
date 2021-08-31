8f69f0021094ba9676b75e597a033dd3ea0669c6
Ondra Kubec <ondra.kubec@icloud.com>
2021-08-31 21:09:54 +0200

    self hosting fonts, vue chunks webpack misconfig debug

--- resources/js/public_SPA/router/routes.js
+++ resources/js/public_SPA/router/routes.js
@@ -1,17 +1,17 @@
 export default [
     {
         path: '/',
-        component: () => import(/* webpackChunkName: "home" */ "../views/Home"),
+        component: () => import(/* webpackChunkName: "js/home" */ "../views/Home"),
         name: 'home'
     },
     {
         path: '/about',
-        component: () => import(/* webpackChunkName: "about" */ "../views/About"),
+        component: () => import(/* webpackChunkName: "js/about" */ "../views/About"),
         name: 'about'
     },
     {
         path: '/:pathMatch(.*)*',
-        component: () => import(/* webpackChunkName: "notFound" */ "../views/NotFound"),
+        component: () => import(/* webpackChunkName: "js/notFound" */ "../views/NotFound"),
         name: '404'
     }
 ];
